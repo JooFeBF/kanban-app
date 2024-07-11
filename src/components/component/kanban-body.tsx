de14b8257725b965b25d3ef2dc84b24ec5a12cb9
MiguelCano-ia <129132845+MiguelCano-ia@users.noreply.github.com>
2024-07-11 17:22:37 -0500

    Falta el modal para las tareas

--- src/components/component/kanban-body.tsx
+++ src/components/component/kanban-body.tsx
@@ -1,13 +1,30 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
+import { ModalSection } from "@/components/ui/modal-section"
+import { useState } from "react";
 
 export function KanbanBody() {
+  const [modal, setModal] = useState(false)
+
+  const toggleModal = () => {
+    setModal(!modal)
+  }
+  
   return (
-    <main className="flex-1 p-6 h-full">
-      <div className="flex justify-end items-center mb-6">
-        <Button>Add Section</Button>
-      </div>
-    </main>
-  )
+    <>
+      <main className="flex-1 p-6 h-full">
+        <div className="flex justify-end items-center mb-6">
+          <Button onClick={toggleModal}>Add Section</Button>
+        </div>
+      </main>
+      {modal && (
+        <div className="fixed inset-0 bg-opacity-30 bg-background-opacity flex justify-center items-center">
+          <ModalSection />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default KanbanBody
