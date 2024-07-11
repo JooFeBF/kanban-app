b62f7e3959795342969e99f8cc277dde2295f426
MiguelCano-ia <129132845+MiguelCano-ia@users.noreply.github.com>
2024-07-11 09:06:14 -0500

    added welcome route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import { Profile } from "@/components/component/profile";
-import { RegisterForm } from "@/components/component/register-form";
-import { LoginForm } from "@/components/component/login-form";
-import { KanbanBody } from "@/components/component/kanban-body";
-import Image from "next/image";
-import { WelcomePage } from "@/components/component/welcome-page";
+import { WelcomePage } from '@/components/component/welcome-page'
 
 export default function Home() {
   return (
-    <main>
-      <KanbanBody></KanbanBody>
-    </main>
+    <body>
+      <WelcomePage />
+    </body>
   );
 }
