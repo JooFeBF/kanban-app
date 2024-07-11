9f81327426739f0516000e479accb1f5775d4018
yeisonbetancur <yeison.betancur@utp.edu.co>
2024-07-11 08:52:24 -0500

    preparacion

---
+++ src/app/kanban/page.tsx
@@ -0,0 +1,9 @@
+import KanbanBody from "@/components/component/kanban-body"
+
+function KanbanPage(){
+  return(
+    <KanbanBody></KanbanBody>
+  )
+}
+
+export default KanbanPage
---
+++ src/components/component/kanban-task.tsx
@@ -0,0 +1,5 @@
+export function KanbanTaks(){
+  return(
+    
+  )
+}
