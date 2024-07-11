c65919c7a745e4bef0dcfb78f757d25fe5440c8b
MiguelCano-ia <129132845+MiguelCano-ia@users.noreply.github.com>
2024-07-11 11:42:14 -0500

    modals added

--- src/app/kanban/layout.tsx
+++ src/app/kanban/layout.tsx
@@ -1,16 +1,18 @@
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
 import Navbar from "@/components/ui/nav";
+import { ModalLogout } from "@/components/ui/modal-logout";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <body className="flex flex-col h-dvh">
+    <section className="flex flex-col h-dvh">
       <Header />
       <div className="flex flex-1 h-full">
         <Navbar />
         {children}
+        <ModalLogout />
       </div>
       <Footer />
-    </body>
+    </section>
   );
 }
