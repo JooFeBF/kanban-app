51a351166fa55656b839bf8287d7f07cd48f1b67
anny <anny@utp.edu.co>
2024-07-11 08:40:51 -0500

    Design fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Profile } from "@/components/component/profile";
+import { RegisterForm } from "@/components/component/register-form";
+import { LoginForm } from "@/components/component/login-form";
+import { KanbanBody } from "@/components/component/kanban-body";
 import Image from "next/image";
+import { WelcomePage } from "@/components/component/welcome-page";
 
 export default function Home() {
   return (
     <main>
-      <Profile />
+      <KanbanBody></KanbanBody>
     </main>
   );
 }
