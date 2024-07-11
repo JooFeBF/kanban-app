bd35462940c1aef5796d385d42955357881fd915
MiguelCano-ia <129132845+MiguelCano-ia@users.noreply.github.com>
2024-07-11 10:30:26 -0500

    fixed routes

--- src/app/kanban/layout.tsx
+++ src/app/kanban/layout.tsx
@@ -4,13 +4,13 @@ import Navbar from "@/components/ui/nav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <body className="flex flex-col h-dvh">
+    <section className="flex flex-col h-dvh">
       <Header />
       <div className="flex flex-1 h-full">
         <Navbar />
         {children}
       </div>
       <Footer />
-    </body>
+    </section>
   );
 }
