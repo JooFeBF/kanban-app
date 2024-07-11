3e01392db7b9931d290243294160784736106546
yeisonbetancur <yeison.betancur@utp.edu.co>
2024-07-11 14:57:47 -0500

    a

--- src/components/component/kanban-section.tsx
+++ src/components/component/kanban-section.tsx
@@ -2,24 +2,18 @@
 import { KanbanTask } from "./kanban-task";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { useGetColumnsQuery } from "@/api/api";
 
 
-export function KanbanSection(){
+export function KanbanSection(props){
+  
 const [tasks, setTasks] = useState([]);
 
 useEffect(() => {
   const fetchTasks = async () => {
     try {
       console.log("1")
-      const response = await axios.get(
-        "https://kanban-con-typescript.onrender.com/api/cards/11",
-        {
-          headers: {
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzksImVtYWlsIjoieWVpQGhvdG1haWwuY29tIiwiaWF0IjoxNzIwNzEwNTA0LCJleHAiOjE3MjA3MzU3MDR9.DoWfD3slFD_-yiovnu6pI82D8O59T18WMuWCO3-Q2As",
-          },
-        }
-      );
+      const response = await useGetColumnsQuery(11);
       console.log("2")
       console.log(response.data);
       setTasks(response.data); // Actualiza el estado con los datos recibidos
