fcdf3f61f2ca707c4403384c16b25bf0f4ff43d0
birulboy <brahian.castro@utp.edu.co>
2024-07-12 21:43:30 -0500

    auto login add

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,13 @@
+"use client"
 import { LoginForm } from "@/components/component/login-form";
-
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
 export default function LoginPage() {
+  const router = useRouter()
+   useEffect(() => {
+    if ( localStorage.token )
+      router.push("/kanban") 
+     } ,  ) ; 
   return (
     <main className="flex">
       <LoginForm />
